9e917df4f35bc8e118aa1ac203a47067bf78de77
李诗 <lishi@youxiatrip.onaliyun.com>
2025-04-30 10:45:50 +0800

    feat: add GitHub Actions workflow for deploying Next.js site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repositoryName = 'bookmark'
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repositoryName}` : '', // 添加 basePath
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '', // 可选，但推荐添加 assetPrefix
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
